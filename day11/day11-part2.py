f6c2beed443dcfdd606962782efe63e1f84e8164
Matthew Presser <superbutters@gmail.com>
2020-12-11 16:34:21 -0500

    Day 11 Part 2 complete

--- day11/day11-part2.py
+++ day11/day11-part2.py
@@ -3,8 +3,11 @@ This program is essentially a modified Conway's Game of Life with the following 
 - period character '.' represents no seat
 - ell character 'L' represents an empty seat
 - hash character '#' represents an occupied seat
+- a "line of sight" refers to a vector in one of eight directions looking out from a given point: up, down, left, right,
+  and the four diagonals at 45 degree angles between those four directions
+- a "neighbor" is the first seat (occupied or empty) in a line of sight starting from a given point
 - an empty seat with zero adjacent occupied neighbors becomes occupied on the next iteration
-- an occupied seat with at least four occupied neighbors becomes empty on the next iteration
+- an occupied seat with at least five occupied neighbors becomes empty on the next iteration
 
 - The program prints out the number of occupied seats once the board has stabilized (see assumptions)
 
@@ -16,6 +19,31 @@ Assumptions:
 import sys
 
 
+def get_next_neighbor(grid, row, col, east_west, north_south, rows, cols):
+    """
+    Searches a grid in a given direction from a given starting point and returns the first neighbor.
+
+    :param grid:
+    :param row: row coordinate of starting point
+    :param col: col coordinate of starting point
+    :param east_west: 1 if east, -1 if west, 0 if neither (true north/south)
+    :param north_south: 1 if south, -1 if north, 0 if neither (true east/west)
+    :param rows: maximum number of rows in the grid
+    :param cols: maximum number of columns in the grid
+    :return: 1 if the neighbor in the given direction is occupied, 0 otherwise
+    """
+    row += north_south
+    col += east_west
+    while 0 <= row < rows and 0 <= col < cols:
+        if grid[row][col] == 0:
+            return 0
+        elif grid[row][col] == 1:
+            return 1
+        row += north_south
+        col += east_west
+    return 0
+
+
 def iterate(grid, rows, cols):
     stable = True
     # Deep copy the values
@@ -24,33 +52,16 @@ def iterate(grid, rows, cols):
         for col in range(0, cols):
             # Only check seats, never empty spaces
             if grid[row][col] != -1:
-                neighbors = 0
-                # Check row above (if it exists)
-                if row - 1 >= 0:
-                    if col - 1 >= 0 and grid[row-1][col-1] == 1:
-                        neighbors += 1
-                    if col + 1 < cols and grid[row-1][col+1] == 1:
-                        neighbors += 1
-                    if grid[row-1][col] == 1:
-                        neighbors += 1
-                # Check row below (if it exists)
-                if row + 1 < rows:
-                    if col - 1 >= 0 and grid[row+1][col-1] == 1:
-                        neighbors += 1
-                    if col + 1 < cols and grid[row+1][col+1] == 1:
-                        neighbors += 1
-                    if grid[row+1][col] == 1:
-                        neighbors += 1
-                # Check current row
-                if col - 1 >= 0 and grid[row][col-1] == 1:
-                    neighbors += 1
-                if col + 1 < cols and grid[row][col+1] == 1:
-                    neighbors += 1
+                neighbors = sum((
+                    get_next_neighbor(grid, row, col, ew_dir, ns_dir, rows, cols) for ew_dir, ns_dir in [
+                        (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)
+                    ]
+                ))
                 # Change next state accordingly
                 if grid[row][col] == 0 and neighbors == 0:
                     next_state[row][col] = 1
                     stable = False
-                elif grid[row][col] == 1 and neighbors >= 4:
+                elif grid[row][col] == 1 and neighbors >= 5:
                     next_state[row][col] = 0
                     stable = False
                 # Else, state was already copied
